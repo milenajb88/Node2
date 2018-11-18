1ff6bdd6b9e0a1215681222a0964206828b2c5d8
Milena.Home <keidy.jimenez@ucrso.info>
2018-11-17 18:14:52 -0600

    Agregar archivos de proyecto.

---
+++ Node2/Models/Block.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Node2.Models
+{
+    public partial class Block
+    {
+        public int Id { get; set; }
+        public string PreviousHash { get; set; }
+        public string Hash { get; set; }
+        public int? IdTicket { get; set; }
+    }
+}
---
+++ Node2/Models/Node2Context.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Node2.Models
+{
+    public partial class Node2Context : DbContext
+    {
+        public Node2Context()
+        {
+        }
+
+        public Node2Context(DbContextOptions<Node2Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Block> Block { get; set; }
+        public virtual DbSet<Ticket> Ticket { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-JTTFEDD\\MSQLSERVER2;Database=Node2;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Ticket>(entity =>
+            {
+                entity.Property(e => e.CreateDate).HasColumnType("datetime");
+            });
+        }
+    }
+}
